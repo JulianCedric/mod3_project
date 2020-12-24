00bbfbb007b239d9ebf6312081ad26ca48bd0049
JulianCedric <julian.pormentilla@gmail.com>
2020-12-24 14:55:53 -0500

    Module 3 - Updates

---
+++ src/ts.js
@@ -0,0 +1,8 @@
+// Think Space: 
+
+// 2020.12.24 - THU:
+
+// One way to run JavaScript code (via Visual Studio Code) is by right clicking on your index.html file and selecting Open with Live Server.
+// This will render your code on your (Chrome) browser.
+
+//
